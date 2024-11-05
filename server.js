4506c0dc9cfc35743293912c0c6f303b32531957
emesonmikael <emesonmikael@gmail.com>
2024-11-05 20:19:38 -0300

    m

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Configuração do proxy
 app.use('/proxy', createProxyMiddleware({
-  target: 'http://conexaoiptv.online:80', // Alvo original (HTTP)
+  target: 'http://cloudtv.fun', // Alvo original (HTTP)
   changeOrigin: true,
   pathRewrite: {
     '^/proxy': '', // Remove /proxy do caminho da URL
